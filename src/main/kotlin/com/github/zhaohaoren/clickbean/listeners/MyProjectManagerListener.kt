a2c045ad41252d80e049c6c62b853f71394a0785
GitHub Action <action@github.com>
2021-02-04 10:30:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhaohaoren/clickbean/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/zhaohaoren/clickbean/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zhaohaoren.clickbean.listeners
 
+import com.github.zhaohaoren.clickbean.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/zhaohaoren/clickbean/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zhaohaoren/clickbean/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhaohaoren.clickbean.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhaohaoren.clickbean.MyBundle
 
 class MyApplicationService {
 
